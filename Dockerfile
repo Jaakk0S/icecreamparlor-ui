ccfe526f910e1ad46f72d9670c8325eb3feea779
Jaakko Saaristo <jaakko.saaristo@gmail.com>
2025-12-16 22:23:18 +0000

    changing prod mode to use server host and port for menu and order api

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,5 @@
 FROM alpine:latest AS base
 
-ENV VITE_MENU_API_HOST=menu-service
-ENV VITE_MENU_API_PORT=8080
-ENV VITE_ORDER_API_HOST=order-service
-ENV VITE_ORDER_API_PORT=8080
-
 RUN apk add --no-cache nodejs curl && apk add --no-cache npm && rm -rf /var/cache/apk/*
 COPY ./ ./
 COPY package*.json .
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-// First look for environment variables. If they're not found, use the test variables in .env
+// In dev mode, use test values in .env. Otherwise use the server host and port
 
-let menuHost: string = import.meta.env.VITE_MENU_API_HOST ? import.meta.env.VITE_MENU_API_HOST : import.meta.env.VITE_MENU_API_TEST_HOST;
-let menuPort: string = import.meta.env.VITE_MENU_API_PORT ? import.meta.env.VITE_MENU_API_PORT : import.meta.env.VITE_MENU_API_TEST_PORT;
-let orderHost: string = import.meta.env.VITE_ORDER_API_HOST ? import.meta.env.VITE_ORDER_API_HOST : import.meta.env.VITE_ORDER_API_TEST_HOST;
-let orderPort: string = import.meta.env.VITE_ORDER_API_PORT ? import.meta.env.VITE_ORDER_API_PORT : import.meta.env.VITE_ORDER_API_TEST_PORT;
+let menuHost: string = import.meta.env.DEV ? import.meta.env.VITE_MENU_API_TEST_HOST : location.hostname;
+let menuPort: string = import.meta.env.DEV ? import.meta.env.VITE_MENU_API_TEST_PORT : location.port;
+let orderHost: string = import.meta.env.DEV ? import.meta.env.VITE_ORDER_API_TEST_HOST : location.hostname;
+let orderPort: string = import.meta.env.DEV ? import.meta.env.VITE_ORDER_API_TEST_PORT : location.port;
 
 export const MENUAPI_ENDPOINT: string = `http://${menuHost}:${menuPort}/menu/v1/`;
 export const ORDERAPI_ENDPOINT: string = `http://${orderHost}:${orderPort}/order/v1/`;
