43f9057579e82f61853f1069b70ae3b7337ac701
Jaakko Saaristo <jaakko.saaristo@gmail.com>
2025-12-23 21:13:54 +0000

    added target blank to links

--- src/components/mainPage.tsx
+++ src/components/mainPage.tsx
@@ -145,7 +145,7 @@ export class MainPage extends Component<MainPageProps, MainPageState> {
           </div>
           <div className="footer">
             This is a free demo app.<br />
-            To browse and test the internal API, click here for <a href={"/swagger/menuapi/"}>Menu API</a> and <a href={"/swagger/orderapi/"}>Order API</a>.
+            To browse and test the internal API, click here for <a href={"/swagger/menuapi/"} target="_blank">Menu API</a> and <a href={"/swagger/orderapi/"} target="_blank">Order API</a>.
           </div>
         </div>
       </main >
